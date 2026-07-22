4ee41a418b2ae9edd62783b9e22707bd72c2c3ea
Rafaela Simioni <rsimionideavila@gmail.com>
2026-07-22 13:00:41 -0300

    chore: estrutura inicial do projeto

---
+++ src/test/java/br/com/rafaelasimioni/raizesdonordeste/RaizesDoNordesteApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.rafaelasimioni.raizesdonordeste;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RaizesDoNordesteApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
