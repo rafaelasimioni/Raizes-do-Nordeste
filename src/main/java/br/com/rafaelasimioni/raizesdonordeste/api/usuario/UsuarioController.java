8b696bdf922dcd55ed8a2cb34c991a7b3a2ec0d6
Rafaela Simioni <rsimionideavila@gmail.com>
2026-07-22 14:51:59 -0300

    chore: estrutura inicial da arquitetura

---
+++ src/main/java/br/com/rafaelasimioni/raizesdonordeste/api/usuario/UsuarioController.java
@@ -0,0 +1,4 @@
+package br.com.rafaelasimioni.raizesdonordeste.api.usuario;
+
+public class UsuarioController {
+}
---
+++ src/main/java/br/com/rafaelasimioni/raizesdonordeste/application/usuario/UsuarioService.java
@@ -0,0 +1,4 @@
+package br.com.rafaelasimioni.raizesdonordeste.application.usuario;
+
+public class UsuarioService {
+}
